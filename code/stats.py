eac580c2dd4a0548da83256a38a9bea1bad2d601
Jim Blomo <jim.blomo+github@gmail.com>
2013-02-08 01:30:49 -0800

    probability and preprocessings slides; lab skeleton

---
+++ code/stats.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+"""This script can be used to analyze data in the 2012 Presidential Campaign,
+available from http://www.fec.gov/disclosurep/PDownload.do"""
+
+import fileinput
+import csv
+
+total = 0
+
+for row in csv.reader(fileinput.input()):
+    if not fileinput.isfirstline():
+        total += float(row[9])
+        ###
+        # TODO: calculate other statistics here
+        # You may need to store numbers in an array to access them together
+        ##/
+
+###
+# TODO: aggregate any stored numbers here
+#
+##/
+
+##### Print out the stats
+print "Total: %s" % total
+print "Minimum: "
+print "Maximum: "
+print "Mean: "
+print "Median: "
+# square root can be calculated with N**0.5
+print "Standard Deviation: "
+
+##### Comma separated list of unique candidate names
+print "Candidates: "
+
+def minmax_normalize(value):
+    """Takes a donation amount and returns a normalized value between 0-1. The
+    normilzation should use the min and max amounts from the full dataset"""
+    ###
+    # TODO: replace line below with the actual calculations
+    norm = value
+    ###/
+    
+    return norm
+
+##### Normalize some sample values
+print "Min-max normalized values: %r" % map(minmax_normalize, [2500, 50, 250, 35, 8, 100, 19])
+
